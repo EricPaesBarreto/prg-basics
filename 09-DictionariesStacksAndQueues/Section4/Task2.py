2d6531548966321e4c0ed3eaea423796878b28df
Eric Paes Barreto <ericpaesbarreto@outlook.com>
2024-12-06 12:38:27 +0100

    Completed section4

--- 09-DictionariesStacksAndQueues/Section4/Task2.py
+++ 09-DictionariesStacksAndQueues/Section4/Task2.py
@@ -8,20 +8,22 @@ while True:
    print('2. Print file')
    print('0. Quit')
    menu = input('Select an option: ')
-   ...
-   ...
+   
    
    if menu == '1':
       file_name = input('Enter file name to print: ')
-      # add new file to the end of the queue 
-   ...
+      files_to_print.put(file_name)
 
-   elif ...:
-      if # print queue not empty
-         file_to_print = ...
-         print(f'File {file_to_print} is currently being printed. Please wait!')
+   elif menu == '2':
+      if not files_to_print.empty():
+        while not files_to_print.empty():
+            file_to_print = files_to_print.get()
+            print(f'\nFile {file_to_print} is currently being printed. Please wait!')
+            with open(file_to_print, 'r') as file:
+               print(file)# pretend this prints the lines of the files
+
       else:
-         print(...)
+        print("No files were added to the queue. Please add some files to print.")
 
    elif menu == '0':
-      break
+    break
